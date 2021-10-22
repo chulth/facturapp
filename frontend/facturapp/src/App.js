db1277c162883c06e82bac4b4de8204ae26f7841
John Bulla <chulth@gmail.com>
2021-10-22 17:53:43 -0300

    Feat: files created autocamically

---
+++ frontend/facturapp/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Grid from './components/Grid';
+
+class App extends Component {
+  render() {
+
+    return (
+      <Grid/>
+    );
+  }
+}
+
+export default App;
